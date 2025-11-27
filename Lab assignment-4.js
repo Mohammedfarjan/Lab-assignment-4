46dddd144d8273f821f51f5b3f951e1c62bef840
Mohammedfarjan <Mohdfarjan680@gmail.com>
2025-11-27 21:36:33 +0530

    Add files via upload

---
+++ Lab assignment-4.js
@@ -0,0 +1,16 @@
+// Define the quiz question and answer
+const Quizquestions = [
+    {
+        question: ""
+    }
+];
+
+// Initialize the score
+let score = 0;
+
+//Loop through each question
+for (let i = 0; i < Quizquestions.length; i++) {
+    // Prompt through each question
+    const userAnswer = prompt(Quizquestions[i].question);
+    
+}
